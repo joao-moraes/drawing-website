b89e2492cfd23dabd2d41df8569fbd9831a79cf0
newClip <103204822+newClip@users.noreply.github.com>
2022-05-31 15:12:40 -0400

    greatly improved code for generating gradient of color picker, also removed tutorial canvas

--- js/main.js
+++ js/main.js
@@ -1,71 +1,18 @@
-let canvas = document.getElementById("tutorial");
-let context = canvas.getContext("2d");
-
-// make 2 rectangles
-context.fillStyle = "rgb(200, 0, 0)";
-context.fillRect(10,10, 50, 50);
-
-context.fillStyle = "rgba(0, 0, 200, 0.5)";
-context.fillRect(30, 30, 50, 50);
-
-
-// this code draws the colors of the main/primary color picker
+// draw the gradient colors of the main/primary color picker
 let primary = document.getElementById("primary");
 let p_ctx = primary.getContext("2d");
+const STOP = 1 / 6;
+let gradient = p_ctx.createLinearGradient(0, 0, 0, primary.height);
 
-let red = 255;
-let green = 0;
-let blue = 0;
-p_ctx.lineWidth = 1.5;
+gradient.addColorStop(0, 'rgb(255, 0, 0)');
+gradient.addColorStop(STOP, 'rgb(255, 255, 0)');
+gradient.addColorStop(2 * STOP, 'rgb(0, 255, 0)');
+gradient.addColorStop(3 * STOP, 'rgb(0, 255, 255)');
+gradient.addColorStop(4 * STOP, 'rgb(0, 0, 255)');
+gradient.addColorStop(5 * STOP, 'rgb(255, 0, 255)');
+gradient.addColorStop(6 * STOP, 'rgb(255, 0, 0)');
 
-for (i = 0; i < 30; i++) {
-    green += i / 2;
-    p_ctx.strokeStyle = "rgb(" + red + "," + green + "," + blue + ")";
-    p_ctx.beginPath();
-    p_ctx.moveTo(0, i);
-    p_ctx.lineTo(50, i);
-    p_ctx.stroke();
-}
-for (i = 0; i < 30; i++) {
-    red -= i / 2;
-    p_ctx.strokeStyle = "rgb(" + red + "," + green + "," + blue + ")";
-    p_ctx.beginPath();
-    p_ctx.moveTo(0, i + 30);
-    p_ctx.lineTo(50, i + 30);
-    p_ctx.stroke();
-}
-for (i = 0; i < 30; i++) {
-    blue += i / 2;
-    p_ctx.strokeStyle = "rgb(" + red + "," + green + "," + blue + ")";
-    p_ctx.beginPath();
-    p_ctx.moveTo(0, i + 60);
-    p_ctx.lineTo(50, i + 60);
-    p_ctx.stroke();
-}
-for (i = 0; i < 30; i++) {
-    green -= i / 2;
-    p_ctx.strokeStyle = "rgb(" + red + "," + green + "," + blue + ")";
-    p_ctx.beginPath();
-    p_ctx.moveTo(0, i + 90);
-    p_ctx.lineTo(50, i + 90);
-    p_ctx.stroke();
-}
-for (i = 0; i < 30; i++) {
-    red += i / 2;
-    p_ctx.strokeStyle = "rgb(" + red + "," + green + "," + blue + ")";
-    p_ctx.beginPath();
-    p_ctx.moveTo(0, i + 120);
-    p_ctx.lineTo(50, i + 120);
-    p_ctx.stroke();
-}
-for (i = 0; i < 30; i++) {
-    blue -= i / 2;
-    p_ctx.strokeStyle = "rgb(" + red + "," + green + "," + blue + ")";
-    p_ctx.beginPath();
-    p_ctx.moveTo(0, i + 150);
-    p_ctx.lineTo(50, i + 150);
-    p_ctx.stroke();
-}
-// up to here for the main-color picker
+p_ctx.fillStyle = gradient;
+p_ctx.fillRect(0, 0, primary.width, primary.height);
 
-primary.addEventListener("mousedown")
+//primary.addEventListener("mousedown")
