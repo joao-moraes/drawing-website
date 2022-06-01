8efca376288485bd1cd38d5ef475397050a2acfa
newClip <103204822+newClip@users.noreply.github.com>
2022-06-01 09:49:47 -0400

    hue is now updating based on primary picked

--- js/main.js
+++ js/main.js
@@ -16,24 +16,31 @@ p_ctx.fillStyle = gradient;
 p_ctx.fillRect(0, 0, primary.width, primary.height);
 
 // update the desired primary color based on mouse click
-primary.addEventListener("mousedown", event)
+primary.addEventListener("mousedown", function(event) {
+    let y = event.offsetY;
+    primaryData = p_ctx.getImageData(1, y, 1, 1);
+    pixelColor = primaryData.data;
 
+    const rgb = `rgba(${pixelColor[0]}, ${pixelColor[1]}, ${pixelColor[2]}, ${pixelColor[3] / 255})`;
+    makeHue(rgb);
+});
 
+// draw and update the hue picker. This function is called when the primary color is selected/changed in the primary color block
+function makeHue(currentColor) {
+    let hue = document.getElementById("hue-sat");
+    let h_ctx = hue.getContext("2d");
 
-// draw and update the hue picker
-let hue = document.getElementById("hue-sat");
-let h_ctx = hue.getContext("2d");
-let currentColor = [255, 0, 0];
-
-// x-axis gradient
-let gradientX = h_ctx.createLinearGradient(hue.width, 0, 0, 0);
-gradientX.addColorStop(0, `rgb(${currentColor[0]}, ${currentColor[1]}, ${currentColor[2]})`);
-gradientX.addColorStop(1, `rgb(255, 255, 255)`);
-h_ctx.fillStyle = gradientX;
-h_ctx.fillRect(0, 0, hue.width, hue.height);
-// y-axis gradient
-let gradientY = h_ctx.createLinearGradient(hue.width, 0, hue.width, hue.height);
-gradientY.addColorStop(0, `rgba(0, 0, 0, 0)`);
-gradientY.addColorStop(1, `rgba(0, 0, 0, 1)`);
-h_ctx.fillStyle = gradientY;
-h_ctx.fillRect(0, 0, hue.width, hue.height);
+    // x-axis gradient
+    let gradientX = h_ctx.createLinearGradient(hue.width, 0, 0, 0);
+    gradientX.addColorStop(0, currentColor);
+    gradientX.addColorStop(1, `rgb(255, 255, 255)`);
+    h_ctx.fillStyle = gradientX;
+    h_ctx.fillRect(0, 0, hue.width, hue.height);
+    
+    // y-axis gradient
+    let gradientY = h_ctx.createLinearGradient(hue.width, 0, hue.width, hue.height);
+    gradientY.addColorStop(0, `rgba(0, 0, 0, 0)`);
+    gradientY.addColorStop(1, `rgba(0, 0, 0, 1)`);
+    h_ctx.fillStyle = gradientY;
+    h_ctx.fillRect(0, 0, hue.width, hue.height);
+}
